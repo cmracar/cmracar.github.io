a0322a41b7022bc3e7ea25daea0aab46d2d0ab6e
Cemre Acar <cemreacar94@gmail.com>
2025-06-20 14:50:22 +0300

    dev for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
+const nextConfig = {
+  output: 'export',
+  basePath: '/cmracar.github.io',
+  assetPrefix: '/cmracar.github.io/',
+}
 export default nextConfig;
